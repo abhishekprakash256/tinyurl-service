64c81ca8acd7f876cdad9d0480f14d62bbb5b16a
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-05-25 21:30:24 -0400

    test code

--- app.py
+++ app.py
@@ -5,34 +5,39 @@ The application uses redis to store the mapping between the short hashes and the
 
 """
 
-from flask import Flask, request, jsonify
+#from flask import Flask,request, jsonify, send_from_directory, abort
 from redis_helper_kit import Helper_fun
-from unique_hash_generator import generate_unique_hash
+from hash_utils import generate_unique_hash
 
 
 
 
 #SET REDIS CONFIGURATION
 REDIS_HOST = 'localhost'
-HASH_NAME = 'TEST_HASH'
-SET_NAME = 'TEST_SET'
+PRIMARY_SET = 'frest_hash_set'
+SECONDRY_SET = 'used_hash_set'
+HASH_NAME = 'tiny_url_hash'
+
 
 
 
 
 #make the redis instance
-redis_helper = Helper_fun(REDIS_HOST, HASH_NAME, SET_NAME)
+redis_helper = Helper_fun(host_name = REDIS_HOST)
 
 # generate a unique hash to store in the redis 
-generate_unique_hash(HASH_NAME, SET_NAME, REDIS_HOST, 5, 10, 100)   #min length 5, max length 10, 100 hashes
+generate_unique_hash(PRIMARY_SET, SECONDRY_SET, REDIS_HOST, 5, 10, 100)   #min length 5, max length 10, 100 hashes
 
 #pop the hash from the redis set
-redis_helper.pop_set_val()
+hash_val = redis_helper.pop_set_val(PRIMARY_SET)
+
+print("hash_val: ", hash_val)
 
 #add the hash and the url to redis
-redis_helper.add_value_to_hash("key1", "value1")
+redis_helper.add_value_to_hash(hash_val, "www.meabhi.me" , HASH_NAME)
 
 #get the value from the hash
-redis_helper.get_hash_value("key1")
+url = redis_helper.get_hash_value(hash_val , HASH_NAME)
 
+print("url: ", url)
 
